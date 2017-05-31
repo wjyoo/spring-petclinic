49342579e5f3e85ec92d656315eedfc6099f1ad5
Felix Barnsteiner <felix.barnsteiner@isys.de>
2017-05-31 11:31:19 +0200

    Update to stagemonitor 0.80.0

--- src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
@@ -15,11 +15,6 @@
  */
 package org.springframework.samples.petclinic.web;
 
-import java.util.Collection;
-import java.util.Map;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
@@ -32,6 +27,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 /**
  * @author Juergen Hoeller
